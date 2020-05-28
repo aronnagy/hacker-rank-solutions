ddcdd9009098634130f38533175c92347175c519
Aron Nagy <39951338+aronnagy@users.noreply.github.com>
2020-05-28 09:11:03 +0200

    Diagonal Difference
    Given a square matrix, calculate the absolute difference between the sums of its diagonals.

---
+++ diagonalDifference.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+function diagonalDifference($arr) {
+    $primary_diagonal = 0;
+    $secondary_diagonal = 0;
+    for($i=0; $i<count($arr); $i++)
+    {
+        for($j=0; $j<count($arr); $j++)
+        {
+            if($i == $j)
+            {
+                $primary_diagonal += $arr[$i][$j];
+            }
+        }
+    }
+    for($i=count($arr)-1; $i>-1; $i--)
+    {
+        for($j=0; $j<count($arr); $j++)
+        {
+               if(abs($i-2) == $j)
+               {
+                   $secondary_diagonal += $arr[abs($i)][$j];
+               }
+        }
+    }
+    return abs($primary_diagonal - $secondary_diagonal);
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$n = intval(trim(fgets(STDIN)));
+
+$arr = array();
+
+for ($i = 0; $i < $n; $i++) {
+    $arr_temp = rtrim(fgets(STDIN));
+
+    $arr[] = array_map('intval', preg_split('/ /', $arr_temp, -1, PREG_SPLIT_NO_EMPTY));
+}
+
+$result = diagonalDifference($arr);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
